19c24e878ebaa642d961a1a4eb23c9ba9bf7e0b5
Brent Cook <busterb@gmail.com>
2017-12-04 06:20:43 -0600

    add mettle_rpc

---
+++ mettle/src/mettle_rpc.h
@@ -0,0 +1,10 @@
+#ifndef _METTLE_RPC_H_
+#define _METTLE_RPC_H_
+
+#include "mettle.h"
+
+void mettle_rpc_free(struct mettle_rpc *mrpc)
+
+struct mettle_rpc * mettle_rpc_new(struct mettle *m)
+
+#endif
