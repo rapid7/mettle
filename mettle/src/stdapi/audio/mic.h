80e19794c60762a89f079d9929faa5995df7765a
Eliott Teissonniere <10683430+DeveloppSoft@users.noreply.github.com>
2018-06-04 12:26:32 +0000

    Remove unused function

--- mettle/src/stdapi/audio/mic.h
+++ mettle/src/stdapi/audio/mic.h
@@ -4,7 +4,6 @@
 struct tlv_packet *audio_mic_list(struct tlv_handler_ctx *ctx);
 struct tlv_packet *audio_mic_start(struct tlv_handler_ctx *ctx);
 struct tlv_packet *audio_mic_stop(struct tlv_handler_ctx *ctx);
-struct tlv_packet *audio_mic_get_frame(struct tlv_handler_ctx *ctx);
 
 ssize_t audio_mic_read(struct channel *c, void *buf, size_t len);
 #endif
