480a8a067d1ac5c41954489d871121e71014debe
Eliott Teissonniere <eliott.teissonniere@gmail.com>
2018-06-05 11:27:28 +0200

    Fix parenthesis error

--- mettle/src/stdapi/audio/mic.h
+++ mettle/src/stdapi/audio/mic.h
@@ -3,10 +3,10 @@
 
 #include "tlv.h"
 
-#define TLV_TYPE_AUDIO_DURATION        (TLV_META_TYPE_UINT   | TLV_EXTENSIONS + 10)
-#define TLV_TYPE_AUDIO_DATA            (TLV_META_TYPE_RAW    | TLV_EXTENSIONS + 11)
-#define TLV_TYPE_AUDIO_INTERFACE_ID    (TLV_META_TYPE_UINT   | TLV_EXTENSIONS + 12)
-#define TLV_TYPE_AUDIO_INTERFACE_NAME  (TLV_META_TYPE_STRING | TLV_EXTENSIONS + 13)
+#define TLV_TYPE_AUDIO_DURATION        ((TLV_META_TYPE_UINT   | TLV_EXTENSIONS) + 10)
+#define TLV_TYPE_AUDIO_DATA            ((TLV_META_TYPE_RAW    | TLV_EXTENSIONS) + 11)
+#define TLV_TYPE_AUDIO_INTERFACE_ID    ((TLV_META_TYPE_UINT   | TLV_EXTENSIONS) + 12)
+#define TLV_TYPE_AUDIO_INTERFACE_NAME  ((TLV_META_TYPE_STRING | TLV_EXTENSIONS) + 13)
 
 struct tlv_packet *audio_mic_list(struct tlv_handler_ctx *ctx);
 struct tlv_packet *audio_mic_start(struct tlv_handler_ctx *ctx);
