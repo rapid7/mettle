1c0209ce2dbff894c10078bfc37b756f39616864
Tim W <timrlw@gmail.com>
2018-10-08 15:11:05 +0800

    attempt to fix #140, double free on arm-iphone-darwin

--- mettle/src/crypttlv.c
+++ mettle/src/crypttlv.c
@@ -84,7 +84,7 @@ void free_tlv_encryption_ctx(struct tlv_encryption_ctx *ctx)
 		free(ctx->key);
 	if (ctx->iv != NULL)
 		free(ctx->iv);
-	return free(ctx);
+	free(ctx);
 }
 
 void * decrypt_tlv(struct tlv_encryption_ctx* ctx, void *p, size_t buf_len)
@@ -128,12 +128,12 @@ void * encrypt_tlv(struct tlv_encryption_ctx* ctx, void *p, size_t buf_len)
 					memcpy(&hdr->tlv, tlv_buf, tlv_len);
 					unsigned char *tlv_data = out_buf + sizeof(struct tlv_xor_header);
 					memset(tlv_data + value_len, pad_len, pad_len);
-					unsigned char result[enc_size];
-					memset(result, 0, enc_size);
 					if (ctx->initialized) {
 						hdr->encryption_flags = htonl(ctx->flag);
 						unsigned char iv[AES_IV_LEN];
 						memcpy(iv, ctx->iv, AES_IV_LEN); // grab iv before enc manipulates it.
+						unsigned char result[enc_size];
+						memset(result, 0, enc_size);
 						if ((length = aes_encrypt(ctx, tlv_data, enc_size, result)) > 0) {
 							memcpy(tlv_data, iv, AES_IV_LEN);
 							memcpy(tlv_data + AES_IV_LEN, result, length);
@@ -142,7 +142,6 @@ void * encrypt_tlv(struct tlv_encryption_ctx* ctx, void *p, size_t buf_len)
 							break;
 						}
 					} else {
-						free(out_buf);
 						ctx->initialized = true;
 					}
 				}
