edd9b14949ccfb51c21239e68c5f819b888314a7
Adam Cammack <adam_cammack@rapid7.com>
2019-01-28 14:16:21 -0600

    Port noexec to use the high-level reflect API

--- libreflect/examples/noexec.c
+++ libreflect/examples/noexec.c
@@ -10,13 +10,11 @@
 
 #include <reflect.h>
 
-int main(int argc, char **argv, char **env)
+int main(int argc, char **argv)
 {
 	int fd;
 	struct stat statbuf;
 	unsigned char *data = NULL; // ELF file
-	struct mapped_elf exe = {0}, interp = {0};
-
 
 	// TODO: network vvvvv
 	if(argc < 2) {
@@ -47,61 +45,9 @@ int main(int argc, char **argv, char **env)
 
 	// TODO: network ^^^^^
 
-	map_elf(data, &exe);
-	munmap(data, statbuf.st_size);
-	if (exe.ehdr == MAP_FAILED) {
-		printf("Unable to map ELF file: %s\n", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-
-	if (exe.interp) {
-		// Load input ELF executable into memory
-		fd = open(exe.interp, O_RDONLY);
-		if(fd == -1) {
-			printf("Failed to open %p: %s\n", exe.interp, strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-
-		if(fstat(fd, &statbuf) == -1) {
-			printf("Failed to fstat(fd): %s\n", strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-
-		data = mmap(NULL, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-		if(data == MAP_FAILED) {
-			printf("Unable to read ELF file in: %s\n", strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-		printf("Mapped ELF interp file in: %s\n", exe.interp);
-
-		close(fd);
-
-		map_elf(data, &interp);
-		munmap(data, statbuf.st_size);
-		if (interp.ehdr == MAP_FAILED) {
-			printf("Unable to map interpreter for ELF file: %s\n", strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-
-	} else {
-		interp = exe;
-	}
-
-
-	// copy and modify our initial argv and env to reuse
-
-	size_t *new_stack = (size_t *)argv - 1;
-
-	// If allocating a new stack, be sure to give it lots of space since the OS
-	// won't always honor MAP_GROWSDOWN:
-	//size_t *new_stack = (void *) (2047 * PAGE_SIZE + mmap(0, 2048 * PAGE_SIZE,
-	//	PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE|MAP_GROWSDOWN, -1, 0));
-	// Or use the high level API:
-	// reflect_execv(data, argv + 1);
-
-	stack_setup(new_stack, argc - 1, argv + 1, env, NULL,
-			exe.ehdr, interp.ehdr);
-
-	jump_with_stack(interp.entry_point, new_stack);
+	// Alternately, on Linux to force using the memfd_create and execveat
+	// sycalls on Linux:
+	// reflect_mfd_execv(data, argv + 1);
+	reflect_execves(data, argv + 1, NULL, (size_t *) argv - 1);
 	return 0;
 }
