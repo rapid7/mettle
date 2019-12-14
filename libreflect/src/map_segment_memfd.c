0c0abe57253e79de1b220ea321334c8a2dc9efd8
Adam Cammack <adam_cammack@rapid7.com>
2019-12-13 20:01:04 -0600

    Add comments

--- libreflect/src/map_segment_memfd.c
+++ libreflect/src/map_segment_memfd.c
@@ -14,6 +14,9 @@
 #include "map_elf.h"
 #include "map_segment.h"
 
+// Map an ELF segment, using a memfd-backed mapping when executable for the
+// broadest Linux attack surface.
+
 int map_segment(struct mapped_elf *obj, ElfW(Phdr) *phdr, const unsigned char *source) {
 	ElfW(Addr) dest;
 	ssize_t written;
@@ -34,7 +37,12 @@ int map_segment(struct mapped_elf *obj, ElfW(Phdr) *phdr, const unsigned char *s
 	len = phdr->p_filesz;
 	page_rounded_len = PAGE_CEIL(phdr->p_memsz + (phdr->p_vaddr % PAGE_SIZE));
 
+	// Executable segments are more likely to work when mapped from an
+	// anonymous file. The act of writing the memory to the memory-backed file
+	// descriptor and then creating a mapping based tricks the kernel into
+	// thinking it is executing a legit file.
 	if (prot & PROT_EXEC) {
+		// NOTE: passing a name of normal stuff that uses memfds may help in the future
 		memfd = syscall(SYS_memfd_create, "", 0);
 		if (memfd == -1) {
 			goto memfd_failed;
@@ -42,6 +50,7 @@ int map_segment(struct mapped_elf *obj, ElfW(Phdr) *phdr, const unsigned char *s
 		if (ftruncate(memfd, PAGE_CEIL(phdr->p_memsz)) == -1) {
 			goto write_failed;
 		}
+		// Write the code from the source executable to the file
 		dprint("write(%d, %p, %08zx)\n", memfd, source, len);
 		written = write(memfd, source, len);
 		if (written != len) {
@@ -49,6 +58,7 @@ int map_segment(struct mapped_elf *obj, ElfW(Phdr) *phdr, const unsigned char *s
 			goto write_failed;
 		}
 
+		// Map the code into place as if we were mapping it from a normal file
 		dprint("mmap(%p, %08zx, %08x)\n", (void *)PAGE_FLOOR(dest), page_rounded_len, prot);
 		mapping = mmap((void *)PAGE_FLOOR(dest), page_rounded_len, prot, MAP_FIXED | MAP_PRIVATE, memfd, 0);
 		if (mapping == MAP_FAILED) {
@@ -57,6 +67,8 @@ int map_segment(struct mapped_elf *obj, ElfW(Phdr) *phdr, const unsigned char *s
 		}
 		close(memfd);
 	} else {
+		// Read-only or read-write mappings don't require anything special.
+		// First is overlay the segment onto the PROT_NONE placeholder map.
 		dprint("mmap(%p, %08zx)\n", (void *)PAGE_FLOOR(dest), page_rounded_len);
 		mapping = mmap((void *)PAGE_FLOOR(dest), page_rounded_len, PROT_WRITE, \
 				MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
@@ -64,10 +76,14 @@ int map_segment(struct mapped_elf *obj, ElfW(Phdr) *phdr, const unsigned char *s
 			dprint("Failed to mmap(): %s\n", strerror(errno));
 			goto memfd_failed;
 		}
+
+		// Initialize the whole thing, but only copy the size on disk out of
+		// the source.
 		memset(mapping, 0, page_rounded_len);
 		dprint("memcpy(%p, %p, %08zx)\n", (void *)dest, source, len);
 		memcpy((void *)dest, source, len);
 
+		// Change to the specified permissions
 		if(mprotect((void *)PAGE_FLOOR(dest), page_rounded_len, prot) != 0) {
 			dprint("Could not mprotect(): %s\n", strerror(errno));
 			goto memfd_failed;
