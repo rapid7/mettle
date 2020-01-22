5a8cbab193d014844d72c0d18309ba24984e0339
Francesco Soncina <phra@users.noreply.github.com>
2020-01-22 15:45:01 +0100

    add username and hostname to session info
    will fix https://github.com/rapid7/metasploit-framework/issues/12875

--- mettle/src/stdapi/sys/config.c
+++ mettle/src/stdapi/sys/config.c
@@ -60,10 +60,20 @@ struct tlv_packet *sys_config_getuid(struct tlv_handler_ctx *ctx)
 	struct tlv_packet *p = tlv_packet_response_result(ctx, TLV_RESULT_SUCCESS);
 
 #ifndef _WIN32
+# ifndef HOST_NAME_MAX
+#  if defined(_POSIX_HOST_NAME_MAX)
+#   define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
+#  elif defined(MAXHOSTNAMELEN)
+#   define HOST_NAME_MAX MAXHOSTNAMELEN
+#  endif
+# endif /* HOST_NAME_MAX */
+	char *username = getlogin() ? getlogin() : "no-user";
+	char hostname[HOST_NAME_MAX] = "no-hostname";
+	gethostname(hostname, HOST_NAME_MAX);
 	p = tlv_packet_add_fmt(p, TLV_TYPE_USER_NAME,
-			"uid=%d, gid=%d, euid=%d, egid=%d",
-			getuid(), getgid(), geteuid(), getegid());
-#endif
+			"%s @ %s (uid=%d, gid=%d, euid=%d, egid=%d)",
+			username, hostname, getuid(), getgid(), geteuid(), getegid());
+#endif /* _WIN32 */
 	return p;
 }
 
