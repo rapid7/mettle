99816cf0e8facde2008bdc25141a392f13eed319
Shelby Pace <shelby_pace@rapid7.com>
2021-03-19 13:34:57 -0500

    opt for dynamically allocated array for result var

--- mettle/src/crypttlv.c
+++ mettle/src/crypttlv.c
@@ -1,4 +1,5 @@
 #include "crypttlv.h"
+#include "log.h"
 
 size_t aes_decrypt(struct tlv_encryption_ctx* ctx, const unsigned char* data, size_t data_len, unsigned char* result)
 {
@@ -132,13 +133,16 @@ void * encrypt_tlv(struct tlv_encryption_ctx* ctx, void *p, size_t buf_len)
 						hdr->encryption_flags = htonl(ctx->flag);
 						unsigned char iv[AES_IV_LEN];
 						memcpy(iv, ctx->iv, AES_IV_LEN); // grab iv before enc manipulates it.
-						unsigned char result[enc_size];
-						memset(result, 0, enc_size);
-						if ((length = aes_encrypt(ctx, tlv_data, enc_size, result)) > 0) {
-							memcpy(tlv_data, iv, AES_IV_LEN);
-							memcpy(tlv_data + AES_IV_LEN, result, length);
-							tlv_len = length + AES_IV_LEN + TLV_MIN_LEN;
-							hdr->tlv.len = htonl(tlv_len);
+						unsigned char *result = calloc(enc_size, 1);
+						if (result) {
+							memset(result, 0, enc_size);
+							if ((length = aes_encrypt(ctx, tlv_data, enc_size, result)) > 0) {
+								memcpy(tlv_data, iv, AES_IV_LEN);
+								memcpy(tlv_data + AES_IV_LEN, result, length);
+								tlv_len = length + AES_IV_LEN + TLV_MIN_LEN;
+								hdr->tlv.len = htonl(tlv_len);
+							}
+							free(result);
 							break;
 						}
 					} else {
